1aaeeb1ecf7eff99a5ca795b915912cac8549bf7
Egii <regidesoftian@gmail.com>
2022-03-24 13:09:21 +0000

    merlinx: Build Fingerprint from coral March
    Signed-off-by: Egii <regidesoftian@gmail.com>

--- lighthouse_merlinx.mk
+++ lighthouse_merlinx.mk
@@ -49,7 +49,7 @@ WITH_GAPPS := true
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR := xiaomi
 TARGET_VENDOR_PRODUCT_NAME := merlinx
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="raven-user 12 SQ1D.220205.003 8069835 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="coral-user 12 SP2A.220305.012 8177914 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := google/raven/raven:12/SQ1D.220205.003/8069835:user/release-keys
+BUILD_FINGERPRINT := coral/coral:12/SP2A.220305.012/8177914:user/release-keys
